eb40d902b37b2ff73d406f0547560217f673b507
Abhishek Pandey <abhishekpandpandey@gmail.com>
2021-07-22 00:02:20 +0530

    first commit

---
+++ Sudoku-solver/display_sudoku.py
@@ -0,0 +1,47 @@
+import tkinter
+
+
+def answer_display(answer: list[list[int]], initial_sudoku: list[list[str]]) -> None:
+    mainwindow = tkinter.Tk()
+    mainwindow.title('Sudoku')
+    mainwindow.geometry('640x480')
+    label = tkinter.Label(mainwindow, text="Solution to your Sudoku", font=('Arial', 14, 'bold'))
+    label.pack(pady=10)
+    canvas = tkinter.Canvas(mainwindow, width=362, height=362)
+    canvas.place(x=140, y=60)
+
+    entry_sudoku = [[None] * 9 for _ in range(9)]
+
+    row_x = 0
+    col_y = 0
+    for row in range(9):
+        if row % 3 == 0:
+            col_y += 1
+        for col in range(9):
+            entry_sudoku[row][col] = tkinter.Entry(canvas, fg='blue',
+                                                   font=('Arial', 12, 'bold'), justify='center'
+                                                   )
+            # entry_sudoku[row][col].config(highlightthickness=1, highlightcolor="red", highlightbackground='black')
+            if initial_sudoku[row][col].isnumeric():
+                entry_sudoku[row][col].config(fg='black')
+            entry_sudoku[row][col].insert(0, answer[row][col])
+            if col % 3 == 0:
+                row_x += 1
+            entry_sudoku[row][col].place(x=row_x, y=col_y, width=40, height=40)
+            row_x = row_x + 40
+
+        col_y = col_y + 40
+        row_x = 0
+
+    canvas.create_line(121, 0, 121, 362, fill='black')
+    canvas.create_line(242, 0, 242, 362, fill='black')
+    canvas.create_line(0, 121, 362, 121, fill='black')
+    canvas.create_line(0, 242, 362, 242, fill='black')
+
+    button1 = tkinter.Button(mainwindow, text='QUIT!', width=50, fg='white', background='black',
+                             command=mainwindow.destroy)
+
+    # button1.place(x=140, y=440)
+    button1.pack(side='bottom', pady=10)
+
+    mainwindow.mainloop()
